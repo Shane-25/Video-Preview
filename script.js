eed4a11ffd147aa6c4755856ba90033d3463cd42
Shane Schuring <shane_s91@hotmail.com>
2021-07-27 15:56:06 -0700

    Video Preview

---
+++ script.js
@@ -0,0 +1,9 @@
+function over(element) {
+  element.play("mouseover");
+}
+function out(element) {
+  element.pause("mouseout");
+}
+function myFunction() {
+  var x = document.getElementById("myVideo").duration;
+}
